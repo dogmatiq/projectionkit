aec98ff138ef395213eec00af651ceeff9e15712
James Harris <mailjamesharris@gmail.com>
2020-02-28 16:52:23 +1000

    Improve documentation of `resource` functions.

--- resource/resource.go
+++ resource/resource.go
@@ -11,7 +11,11 @@ import (
 // resource operation.
 var ErrNotSupported = errors.New("the handler does not support this operation")
 
-// StoreVersion sets the version of the resource r to v.
+// StoreVersion unconditionally sets the version of the resource r to v.
+//
+// Care should be taken using this function, as it bypasses the
+// optimistic-concurrency checks normally present when updating a resource
+// version. It's almost always better to use UpdateVersion() instead.
 //
 // It returns ErrNotSupported if the handler does not support storing resource
 // versions outside of when an event is handled.
@@ -66,6 +70,14 @@ func UpdateVersion(
 
 // DeleteResource removes all information about the resource r from the
 // handler's data store.
+//
+// For some handler implementations, this is *likely* to be an equivalent
+// operation to h.CloseResource(), however handler implementations are not
+// *required* to remove data about a resource when it is closed.
+//
+// Furthermore, the result of deleting a resource should be that the handler
+// will behave as though it had never had such a resource, whereas the behavior
+// of a projection when using a closed resource is undefined.
 //
 // It returns ErrNotSupported if the handler does not support deleting
 // resources.
