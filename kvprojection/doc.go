6bc3f444d037694fc54c918a284f6639a6c5406d
James Harris <mailjamesharris@gmail.com>
2021-08-06 15:18:23 +1000

    Remove `kvprojection`.

--- kvprojection/doc.go
+++
@@ -1,6 +0,0 @@
-// Package kvprojection provides utilities for building projections based on a
-// very simple transactional key/value store abstraction.
-//
-// Several of the other database-specific packages within the projectionkit
-// module provide implementations of the key/value store.
-package kvprojection
