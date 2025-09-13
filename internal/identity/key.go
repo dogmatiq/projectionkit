5ea5b5b0c636dc0dff6ed800614fb3c752ef7dcb
James Harris <contact@jamesharris.id.au>
2025-09-14 07:25:01 +1000

    Update internal package to support new Dogma interfaces.

--- internal/identity/key.go
+++ internal/identity/key.go
@@ -19,7 +19,6 @@ type configurer struct {
 	key string
 }
 
-func (c *configurer) Identity(_ string, key string)                 { c.key = key }
-func (c *configurer) Routes(...dogma.ProjectionRoute)               {}
-func (c *configurer) DeliveryPolicy(dogma.ProjectionDeliveryPolicy) {}
-func (c *configurer) Disable(...dogma.DisableOption)                {}
+func (c *configurer) Identity(_ string, key string)   { c.key = key }
+func (c *configurer) Routes(...dogma.ProjectionRoute) {}
+func (c *configurer) Disable(...dogma.DisableOption)  {}
